08ef46a04685c932caf5d3012c9b3ecac60b0427
gabgitt <gabrieloyetunde@gmail.com>
2016-05-25 20:46:49 +0100

    latest version

--- E-voting/php/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if(session_destroy()) { // destroying all session
-header("Location:../html/index.php"); // Redirecting to home page
-}
-?>
--- E-voting/php/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-$myemail = "";
-session_start();
-	if(!isset($_SESSION['login_user'])){
-		header("Location:index.php");
-	}else{
-		$myemail = $_SESSION['login_user'];
-}
-?>
