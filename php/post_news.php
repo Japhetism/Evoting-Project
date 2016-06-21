308bed6e506bfe2c5b2f9b3f312e1c154f32f3be
gabgitt <gabrieloyetunde@gmail.com>
2016-06-21 20:29:12 +0100

    postnews purely in PDO

--- php/post_news.php
+++ php/post_news.php
@@ -49,23 +49,24 @@ if(!empty($_POST["post_submit"]) && isset($_POST["post_submit"]))
 //fetching and displaying the news posted by the admin
 $posted_news = $adminPhoto = $profile1 = "";
 $images_dir = "../images/users/";
-$news_query = "SELECT * FROM news WHERE election_id='$election_id' ORDER BY date_created DESC";
 $user_id = getAllMembers("election",["user_id"],["election_id","=",$election_id]);
 $user_id = $user_id[0]["user_id"];
-$admin_photo = "SELECT fname, lname, picture_name FROM users WHERE user_id='$user_id'";
-$result_admin = mysqli_query($connection2,$admin_photo);
-    $row1 = mysqli_fetch_assoc($result_admin);
-        if($result = mysqli_query($connection2, $news_query)){
-            while($row = mysqli_fetch_assoc($result)){
-                $add = $row['news_id'];
-                $adminPhoto = $images_dir.$row1['picture_name'];
-                $date_time = explode(" ", $row['date_created']);
-                $date = getDateInterval($date_time[0]);
-                $time = timeString($date_time[1]);
-                $posted_news.="<div class='me' style='margin-bottom:10px;' >"."<br><div id='dem$add' style='padding-left:2%;'>
-                                <label id=$add style='overflow:hidden;text-overflow:ellipsis;'>".$row['news']."</label>
+$row1 = getAllMembers("users",["fname","lname","picture_name"],["user_id","=",$user_id])[0];
+$news_query = $connection1->prepare("SELECT * FROM news WHERE election_id = '$election_id' ORDER BY date_created DESC");
+$news_query->execute();
+$news_query->setFetchMode(PDO::FETCH_ASSOC);
+$row = $news_query->fetchAll();
+for ($i = 0 ; $i < count($row) ; $i++)
+{
+    $add = $row[$i]['news_id'];
+    $adminPhoto = $images_dir.$row1['picture_name'];
+    $date_time = explode(" ", $row[$i]['date_created']);
+    $date = getDateInterval($date_time[0]);
+    $time = timeString($date_time[1]);
+    $posted_news.="<div class='me' style='margin-bottom:10px;' >"."<br><div id='dem$add' style='padding-left:2%;'>
+                                <label id=$add style='overflow:hidden;text-overflow:ellipsis;'>".$row[$i]['news']."</label>
                                 <br>".$date."&nbsp".$time."&nbsp<br>";
-                $posted_news.="<a href='#' onclick='editNews($add)'>Edit</a>&nbsp&nbsp<a href='#' onclick='deleteNews($add)'>Delete</a>
+    $posted_news.="<a href='#' onclick='editNews($add)'>Edit</a>&nbsp&nbsp<a href='#' onclick='deleteNews($add)'>Delete</a>
                                 </div><div id='other$add'><form method='post'><textarea name='modify_news' id='modify_news$add'
                                 style='display:none; margin-bottom:5px;min-width:90%;margin-left:1%;'></textarea>
                                 <input type='submit' value='Update News' name='update_news' id='update_news$add'
@@ -74,15 +75,8 @@ $result_admin = mysqli_query($connection2,$admin_photo);
                                 <input type='button' value='Cancel' id='cancel_delete_news$add' onclick='cancelNews($add)'
                                 style='display: none;margin-left:40%;'><input type='text' name='id' id='getid$add'
                                 style='display:none'></form></div></div>";
-            }
-        }
 
-
-
-
-
-
-
+}
 
 //getting the news_id from the URL
 $news_id = "";
@@ -90,40 +84,21 @@ if(empty($_POST['id'])){
 
 }else{
     $news_id = $_POST['id'];
-
-    /*if(isset($_GET['under'])){
-        $new_id = $_GET['under'];
-        $news_id= substr($new_id,9,strlen($new_id)-17);*/
     //check if the $news_id is actually an id for a particular news for the election,else redirect to postnews.php with the latest key value
     if(isset($_SESSION['election_id'])){
         $nwElection_id = $_SESSION['election_id'];
-        $election_id=substr($nwElection_id,9,strlen($nwElection_id)-17);
-
+        $election_id = unwrap($nwElection_id);
     }
-    $check_news_query="SELECT * FROM news WHERE news_id='$news_id' AND election_id='$election_id'";
-    $news= mysqli_fetch_row(mysqli_query($connection2,$check_news_query));
-    if(empty($news)){
+    $news = getAllMembers("news",["*"],["news_id","=",$news_id],0,"AND",["election_id","=",$election_id]);
+    if( count($news) == 0){
         header("Location:postnews.php?key=".$nwElection_id);
     }
 }
-/*}*/
+
 
 
 //fetching those posted news by admin to modify
-$query_news = "SELECT news FROM news WHERE news_id='$news_id'";
-if($result_news = mysqli_query($connection2, $query_news)){
-    /*fetch associative array */
-    while($row = mysqli_fetch_assoc($result_news)){
-        $news_fetched = $row['news'];
-    }
-}
-
-
-
-//getting election_id from the saved session
-/*if(isset($_SESSION['election_id'])){
-    $key = $_SESSION['election_id'];
-}*/
+$news_fetched = getAllMembers("news",["news"],["news_id","=",$news_id])[0]["news"];
 
 //posting the update of news
 $post_update = $updateErr = "";
@@ -137,7 +112,7 @@ if(!empty($_POST['update_news']) && isset($_POST['update_news'])){
 
     if(!$updateErr){
         $query_update = "UPDATE news SET news='$post_update' WHERE news_id='$news_id'";
-        if(mysqli_query($connection2, $query_update)){
+        if($connection1->query($query_update)){
             header("Location:postnews.php?key=".$key);
         }else{
 
@@ -146,25 +121,13 @@ if(!empty($_POST['update_news']) && isset($_POST['update_news'])){
 
 }
 
-//getting election_id from the saved session
-/*if(isset($_SESSION['election_id'])){
-    $nwElection_id = $_SESSION['election_id'];
-}*/
-
-
 //deleting news from the database
 $post_update = $updateErr = "";
 if(!empty($_POST['delete_news']) && isset($_POST['delete_news'])){
     $updateErr = false;
-    /*if(empty($_POST['post_news'])){
-        $updateErr = true;
-    }else{
-        $post_update = $_POST['post_news'];
-    }*/
-
     if(!$updateErr){
         $query_delete = "DELETE FROM news WHERE news_id='$news_id'";
-        if(mysqli_query($connection2, $query_delete)){
+        if($connection1->query($query_delete)){
             header("Location:postnews.php?key=".$key);
         }else{
 
@@ -178,60 +141,59 @@ if(!empty($_POST['cancel_delete_news']) && isset($_POST['cancel_delete_news'])){
 }
 
 
-
 //fetching to the view contestant page
 $election_id = "";
 if(isset($_GET['key'])){
     $view_election_id = $_GET['key'];
-    $election_id= substr($view_election_id,9,strlen($view_election_id)-17);
+    $election_id= unwrap($view_election_id);
     $_SESSION['election_id_view'] = $election_id;
 }
 
 //querying for news
 $view_posted_news = "";
-$view_news_query = "SELECT * FROM news WHERE election_id='$election_id' ORDER BY date_created DESC";
-if($result_admin = mysqli_query($connection2,$admin_photo)){
-    while($row1 = mysqli_fetch_assoc($result_admin)){
-        if($view_result = mysqli_query($connection2, $view_news_query)){
-            while($row=mysqli_fetch_assoc($view_result)){
-                $adminPhoto = $images_dir.$row1['picture_name'];
-                $date_time1 = explode(" ", $row['date_created']);
-                $date1 = getDateInterval($date_time1[0]);
-                $time1 = timeString($date_time1[1]);
-                $view_posted_news.="<div class='me' style='margin-bottom:10px; ' >"."<br><label style='overflow:hidden;text-overflow:ellipsis;'>".$row['news']."</label><br>".$date1."&nbsp".$time1."<br></div>";
-            }
-        }
-    }
+$row1 = getAllMembers("users",["fname","lname","picture_name"],["user_id","=",$user_id])[0];
+
+$view_news_query = $connection1->prepare("SELECT * FROM news WHERE election_id = '$election_id' ORDER BY date_created DESC");
+$view_news_query->execute();
+$view_news_query->setFetchMode(PDO::FETCH_ASSOC);
+$row = $view_news_query->fetchAll();
+
+for ($i = 0 ; $i < count($row) ; $i++)
+{
+    $adminPhoto = $images_dir.$row1['picture_name'];
+    $date_time1 = explode(" ", $row[$i]['date_created']);
+    $date1 = getDateInterval($date_time1[0]);
+    $time1 = timeString($date_time1[1]);
+    $view_posted_news.="<div class='me' style='margin-bottom:10px; ' >"."<br>
+                <label style='overflow:hidden;text-overflow:ellipsis;'>".$row[$i]['news']."</label>
+                <br>".$date1."&nbsp".$time1."<br></div>";
 }
 
 //querying for election name
 $election_name = $election_details_test = "";
-$view_election_query = "SELECT * FROM election  WHERE election_id = '$election_id'";
-if($view_election = mysqli_query($connection2, $view_election_query)){
-    while($row = mysqli_fetch_assoc($view_election)){
-        $election_name .= $row['election_name'];
-        $_SESSION['election_name']=$election_name;
-        $election_details_test .= "<label>Start Date:</label> ".dateString($row['election_start_date'])."<br><label>Start Time:</label> ".timeString($row['election_time_from'])."<br><label>End Date:</label> &nbsp".
-            dateString($row['election_end_date'])."<br><label>End Time:</label> ".timeString($row['election_time_to'])."<br><label>Number of Voters: </label> ".count(getAllMembers("joined", ['election_id'], ['election_id', '=', $election_id])).
+$row = getElectionDetails($election_id)[0];
+$election_name .= $row['election_name'];
+$_SESSION['election_name'] = $election_name;
+$election_details_test .= "<label>Start Date:</label> ".dateString($row['election_start_date'])."<br>
+        <label>Start Time:</label> ".timeString($row['election_time_from'])."<br><label>End Date:</label> &nbsp".
+        dateString($row['election_end_date'])."<br><label>End Time:</label> ".timeString($row['election_time_to'])."<br>
+        <label>Number of Voters: </label> ".count(getAllMembers("joined", ['election_id'], ['election_id', '=', $election_id])).
         "<br><label>Number of Contestants: </label> ".count(getAllMembers("contestants", ['contestant_id'], ['election_id', '=', $election_id]));
-    }
-}
-
 
 //getting the user_id for a particular election
 $election_admin_details = $election_admin_detail = "";
 $images_dir = "../images/users/";
-$election_user_id = "SELECT user_id FROM election WHERE election_id = '$election_id'";
-$user_id_result = mysqli_fetch_row(mysqli_query($connection2, $election_user_id));
+$user_id_result = $row["user_id"];
 
 //querying to get the admin email
-$view_user = "SELECT * FROM  users WHERE user_id = '$user_id_result[0]'";
-if($view_user_name = mysqli_query($connection2, $view_user)){
-    while($row = mysqli_fetch_assoc($view_user_name)){
-        $election_admin_details .= "<div class='col-md-6'><label>Name:</label> ".$row['lname']."&nbsp".$row['fname']."<br><label>Username:</label> ".$row['username']."<br><label>Email:</label> ".$row['email']."<br><label>Telephone:</label> ".$row['phone']."</div>";
-        $election_admin_detail .= "<div class='col-md-4'><img src=".$images_dir.$row['picture_name']." width='100px' height='100px' style='border-radius:100%;' id='displayedPhoto'></div>";
-    }
-}
+$row = getAllMembers("users",["*"],["user_id","=",$user_id_result])[0];
+$election_admin_details .= "<div class='col-md-6'><label>Name:</label> ".$row['lname']."&nbsp".$row['fname']."
+        <br><label>Username:</label> ".$row['username']."<br><label>Email:</label> ".$row['email']."<br>
+        <label>Telephone:</label> ".$row['phone']."</div>";
+$election_admin_detail .= "<div class='col-md-4'><img src=".$images_dir.$row['picture_name']." width='100px'
+        height='100px' style='border-radius:100%;' id='displayedPhoto'></div>";
+
+
 
 //checking for the link vote
 $hasvoted=hasvoted(user_id($myemail), $election_id);
