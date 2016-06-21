03c9e6f12f1316e50b16010a4738c1c3a4cf0bc2
gabgitt <gabrieloyetunde@gmail.com>
2016-06-21 21:24:39 +0100

    no more mysqli connection

--- php/connection.php
+++ php/connection.php
@@ -5,33 +5,19 @@
  * Date: 1/22/16
  * Time: 6:24 PM
  */
-function Connection($type, $host, $username='root', $password, $dbname) {
-    if($type==='PDO') {
-        $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => true);
-        try {
-            $dbh = 'mysql:host=' .$host . '; dbname=' . $dbname;
-            return new PDO($dbh, $username, $password, $options);
-        }
-        catch(PDOException $e) {
-            return $e->getMessage();
-        }
-    }
-    elseif($type==='mysqli') {
-        try{
-            return mysqli_connect($host, $username, $password, $dbname);
+function Connection($host, $username='root', $password, $dbname) {
 
-        }
-        catch (mysqli_sql_exception $a) {
-            return $a->getMessage();
-        }
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => true);
+    try {
+        $dbh = 'mysql:host=' .$host . '; dbname=' . $dbname;
+        return new PDO($dbh, $username, $password, $options);
+    }
+    catch(PDOException $e) {
+        return $e->getMessage();
+    }
 
-    }
-    else {
-         die('No connection to Database');
-    }
 }
 $password="eminence";
 $dbname="oluwaranti";
 
-$connection1 = Connection('PDO','localhost','root',$password,$dbname);
-$connection2 =  Connection('mysqli','localhost','root',$password,$dbname);
+$connection1 = Connection('localhost','root',$password,$dbname);
