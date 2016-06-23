0c481387d2eb58643f22df0254975a1ccd6d60a5
gabgitt <gabrieloyetunde@gmail.com>
2016-06-23 20:30:29 +0100

    this is the conn that works with the db

--- php/connection.php
+++ php/connection.php
@@ -5,19 +5,17 @@
  * Date: 1/22/16
  * Time: 6:24 PM
  */
-function Connection($host, $username='root', $password, $dbname) {
+$servername = "127.0.0.1";
+$username = "";
+$password = "";
 
-    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_PERSISTENT => true);
-    try {
-        $dbh = 'mysql:host=' .$host . '; dbname=' . $dbname;
-        return new PDO($dbh, $username, $password, $options);
-    }
-    catch(PDOException $e) {
-        return $e->getMessage();
-    }
-
+try {
+    $connection1 = new PDO("mysql:host=$servername;dbname=dbname", $username, $password);
+    // set the PDO error mode to exception
+    $connection1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
 }
-$password="eminence";
-$dbname="oluwaranti";
-
-$connection1 = Connection('localhost','root',$password,$dbname);
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
--- php/try2.php
+++ php/try2.php
@@ -9,17 +9,3 @@ include ('connection.php');
 include_once('csv.php');
 include_once('database.php');
 include_once('function.php');
-function shit()
-{
-    global $connection1;
-    $getElectionDetails=$connection1->prepare("SELECT * FROM election WHERE election_id=''");
-    $getElectionDetails->execute();
-    $getElectionDetails->setFetchMode(PDO::FETCH_ASSOC);
-    $election_details1=$getElectionDetails->fetchAll();
-    return $election_details1;
-
-}
-$election_id_check_query="SELECT * FROM election WHERE election_id=1";
-$election_id_check= mysqli_query($connection2,$election_id_check_query);
-$election_details= mysqli_fetch_row($election_id_check);
-print_r(getElectionDetails(1));
